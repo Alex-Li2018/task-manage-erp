878c425ce4db5c9235801159933c3248a2979549
alex <alexlismile@163.com>
2021-01-14 17:05:52 +0800

    feat: 代码优化

--- work/pc/.eslintrc.js
+++ work/pc/.eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
           ['@api', './src/api'],
           ['@util', './src/utils'],
           ['@components', './src/components'],
-          ['@mixin', './src/mixins']
+          ['@mixin', './src/mixins'],
         ],
         extensions: ['.vue', '.js', '.json'],
       },
@@ -47,22 +47,23 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    'comma-dangle': ['error', 'only-multiline'],
+    'padded-blocks': 0,
+    'one-var': 0,
+    'no-return-assign': 0,
+    // 禁止无用表达式 关闭
+    'no-unused-expressions': 0,
+    'no-param-reassign': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0,
+    'no-useless-call': 0,
     'vue/max-len': 0,
     'max-len': 0,
-    'vue/script-indent': ['error', 4, {
-      baseIndent: 1,
-    }],
-    'vue/html-indent': ['error', 4, {
-      attribute: 1,
-      baseIndent: 1,
-      closeBracket: 0,
-      alignAttributesVertically: true,
-      ignores: [],
-    }],
-    /*= ==================【关闭一些规则】================ */
-    'vue/prop-name-casing': 0,
-    /* 关闭属性链接符校验 (uni-app中使用会报错) */
-    'vue/attribute-hyphenation': 0,
   },
   overrides: [
     {

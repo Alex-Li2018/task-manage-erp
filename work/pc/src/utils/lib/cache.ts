807d38d4d7c8056f26ebb507eb04338efe43a411
alex <alexlismile@163.com>
2021-01-30 17:59:32 +0800

    feat: 增加本地缓存

---
+++ work/pc/src/utils/lib/cache.ts
@@ -0,0 +1,91 @@
+import { extend } from './util';
+
+const cache = {
+    /**
+     * 获取缓存
+     * @param key   键
+     */
+    get(key: string): any {
+        const name = this.__encrypt(key),
+            data = localStorage.getItem(name);
+        return data ? JSON.parse(data)._data : data;
+    },
+
+    /**
+     * 设置缓存
+     * @param key   键
+     * @param value 值
+     * @param hours 有效时间
+     */
+    set(key: string, value: unknown, hours = 48) {
+        const name = this.__encrypt(key),
+            timeout = new Date().getTime() + hours * 3600 * 1000;
+        localStorage.setItem(name, JSON.stringify({
+            __timeout: timeout,
+            __data: value
+        }));
+    },
+
+    /**
+     * 设置自定义超时缓存
+     * @param key       键
+     * @param value     值
+     * @param timeout   过期时间
+     */
+    setWithTimeout(key: string, value: any, timeout: number) {
+        const name = this.__encrypt(key);
+        localStorage.setItem(name, JSON.stringify({
+            __timeout: timeout,
+            __data: value
+        }));
+    },
+
+    /**
+     * 清除缓存(空清空所有缓存)
+     * @param key
+     */
+    remove(key: string) {
+        const name = this.__encrypt(key);
+        if (arguments.length < 1 || !key) {
+            localStorage.clear();
+        } else {
+            localStorage.removeItem(name);
+        }
+    },
+
+    /**
+     * @private 加密key
+     */
+    __encrypt(name: string): string {
+        return `cache_${name}`;
+    },
+
+    /**
+     * @public 验证超时
+     */
+    checkTimeout(key: string) {
+        const name = this.__encrypt(key),
+            nowTime = new Date().getTime();
+
+        let data : any = localStorage.getItem(name);
+
+        try {
+            data = data ? JSON.parse(data) : data;
+        } catch (e) {
+            return true;
+        }
+        if (Object.prototype.toString.call(data) !== '[object Object]') {
+            if (!data || !(data as object).__data) return true;
+
+            return nowTime > (<object>data).__timeout;
+        } else {
+            return false;
+        }
+    },
+
+    extend(copy: any) {
+        return extend(this, copy, this);
+    }
+};
+
+export default cache;
--- work/pc/src/utils/lib/util.ts
+++ work/pc/src/utils/lib/util.ts
@@ -6,6 +6,45 @@ export function getPageTitle(pageTitle: string):string {
     return `${title}`;
 }
 
+function isArray(val : unknown): boolean {
+    return toString.call(val) === '[object Array]';
+}
+
+export function forEach(obj: any, fn) {
+    if (obj === null || typeof obj === 'undefined') {
+        return;
+    }
+
+    if (typeof obj !== 'object') {
+        obj = [obj];
+    }
+
+    if (isArray(obj)) {
+        for (var i = 0, l = obj.length; i < l; i++) {
+            fn.call(null, obj[i], i, obj);
+        }
+    } else {
+        for (var key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                fn.call(null, obj[key], key, obj);
+            }
+        }
+    }
+}
+
+export function extend(a : any, b: any, thisArg: any) {
+    forEach(b, function assignValue(val: any, key: any) {
+        if (thisArg && typeof val === 'function') {
+            a[key] = val.bind(thisArg);
+        } else {
+            a[key] = val;
+        }
+    });
+    return a;
+}
+
 export default {
-    getPageTitle
+    getPageTitle,
+    forEach,
+    extend
 };
